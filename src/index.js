858e584d9fbf4f0cf25107da437246f0d56e279c
Naor Leizer <naor0naor@gmail.com>
2023-03-19 20:37:08 +0200

    added info to main page added copy to clipboard to mail and phone number added toast notifications (bootstrap)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import ClipboardJS from "clipboard";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+var clipboard = new ClipboardJS(document.querySelectorAll("a"));
+
+var bootstrap = require("bootstrap");
+function attachToast(toastTrigger) {
+  const toastLiveExample = document.getElementById("copiedToast");
+  if (toastTrigger) {
+    toastTrigger.addEventListener("click", () => {
+      const toast = new bootstrap.Toast(toastLiveExample);
+
+      toast.show();
+    });
+  }
+}
+attachToast(document.getElementById("emailClipboardButton"));
+attachToast(document.getElementById("phone"));
+
+// function copyToClipboard(text) {
+//     navigator.clipboard.writeText(text);
+// }
+
+// document.getElementById("emailClipboardButton").onclick = copyToClipboard("naorleizer@gmail.com");
+// document.getElementById("phoneCopy").onclick = copyToClipboard("0526602590");
